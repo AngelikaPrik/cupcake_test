2424bfcbd8d86e636e1e7e47068a11a567c2d661
Angelina Mamakova <pretzel318@gmail.com>
2023-06-29 17:28:26 +0400

    added test values for last 3 strokes

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ const initialState: IMarketDataType = {
   'RUB/CUPCAKE': [0, 0, 0],
   'USD/CUPCAKE': [0, 0, 0],
   'EUR/CUPCAKE': [0, 0, 0],
+  'RUB/USD': [0, 0, 0],
+  'RUB/EUR': [0, 0, 0],
+  'EUR/USD': [0, 0, 0],
 }
 
 const endpoints = [
@@ -32,6 +35,9 @@ function App() {
           'RUB/CUPCAKE': roundValues(responses.map(({ data }) => data.rates.RUB)),
           'USD/CUPCAKE': roundValues(responses.map(({ data }) => data.rates.USD)),
           'EUR/CUPCAKE': roundValues(responses.map(({ data }) => data.rates.EUR)),
+          'RUB/USD': roundValues(responses.map(({ data }) => data.rates.RUB / data.rates.USD)),
+          'RUB/EUR': roundValues(responses.map(({ data }) => data.rates.RUB / data.rates.EUR)),
+          'EUR/USD': roundValues(responses.map(({ data }) => data.rates.EUR / data.rates.USD)),
         }
 
         setMarketData(data)
